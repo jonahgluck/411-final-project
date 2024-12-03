360f9bb9887db9fd81bbc02b0c9ab1b5b963a993
katyabalin <katya.balin@gmail.com>
2024-12-03 14:41:26 -0500

    added docstring for setup_logging function

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,13 @@
 import logging
 
 def setup_logging():
+    """Initializes logging configuration for the application.
+
+        Configures logging with an INFO log level and a custom format that includes a 
+        timestamp and the log message.
+
+        Returns: 
+            None
+    """
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
